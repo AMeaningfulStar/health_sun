565138b75d5db2229452f9d1a8742a9d1b69b0dd
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2023-04-14 11:07:07 +0900

    Ms (#6)
    * FIX: Logo div 태그에서 Link 태그로 변경
    
    23.03.24
    - Logo에 사용하던 div 태그를 Link 태그로 변경
    - Logo 클릭 시 임시로 Login 페이지로 이동
    
    * FIX: CSS의 단위를 vw, vh에서 px로 변경
    
    23.03.31
    - CSS의 단위를 화면 비율에 맞춰 변하도록 vw, vh 단위를 사용하였지만, 웹을 여는 컴퓨터마다 크기가 변하고 비율과 위치가 맞지 않아 px 단위로 크기와 위치를 고정
    
    * FEAT: Main 페이지인 HOME 안에 4개의 박스 틀 생성
    
    23.03.31
    - Main 페이지인 HOME.js 안에 들어가는 4개의 박스 틀 구현
    
    * FEAT: Sidebar에 Logout 추가
    
    23.04.05
    
    - Sidebar 안에 디자인과 같이 Logout 버튼 구현
    - Logout 버튼에 들어가는 image 파일 DesignFile 폴더로 정리
    - SidebarData 안에 잘못된 경로 수정
    
    * FEAT: Main 화면에 내복약 정보 요소 구현
    
    23.04.06
    
    - MedicineInfo 컨포넌트를 만들어 Main 화면에 요소인 내복약 정보 구현
    - 퍼블리싱 단계만 했기때문에 프론트 작업에서 추가 작업 필요
    - 요소의 너비가 넓다고 생각이 되어 너비 수정
    - Sidebar에서 사용하는 화살표의 크기 수정
    
    * FEAT: Main 화면에 예방 접종 추천 요소 구현
    
    23.04.06
    
    - Swiper 패키지 추가
    - Swiper 패키지를 이용하여 배너 스크롤 구현
    - VaccinationAdvise 컨포넌트로 예방 접종 추천 요소 구현
    - Sidebar의 로그아웃 img 태그 안에 alt 추가하여 경고 수정
    
    * FEAT: Main 화면에 나의 건강 상태 요소 구현
    
    23.04.07
    
    - MyHealthCondition 컨포넌트로 Main 화면에 나의 건강 상태 요소 구현
    - react-minimal-pie-chart 패키지를 이용하여 요소에 사용하는 파이 차트 구현
    - VaccinationAdvise 컨포넌트 스타일 추가
    
    * FEAT: Main 화면에 건강 일정 요소 구현
    
    23.04.08
    
    - HealthSchedule 컨포넌트로 Main 화면에 건강 일정 요소 구현
    - 달력 퍼블리싱 구현
    - 월별 이동으로 인한 연도 변경 기능 추가 필요
    - 기능적인 부분 추가 작업 필요
    
    * FIX: Main 화면에 건강 일정, 내복약 정보 요소 스타일 변경
    
    23.04.08
    
    - Main 화면에 건강 일정, 내복약 정보 요소 height 값 변경
    - Main 화면에 내복약 정보 요소 top 값 변경
    
    * FEAT: MyPage 컨포넌트 생성 및 이동 기능 구현
    
    23.04.08
    
    - Header에서 '나의 프로필' 버튼을 클릭 시 MyPage로 이동하는 기능 구현
    - MyPage 컨포넌트 생성
    - MyPage로 이동할 수 있도록 Route 추가
    
    * FEAT: 퍼블리싱
    
    23.04.14
    
    - MainLayout 사용하는 곳에 따라 다르게 return 하도록 구현
    - MedicalExamination 컴포넌트 건강검진 페이지 구현
    - Medicine 컴포넌트 복약정보 페이지 구현
    - Position 컴포넌트 병원 및 약국 찾기 페이지 구현
    - Vaccination 컴포넌트 예방 접종 페이지 구현

--- src/components/PageComponent/Medical_Examination/MedicalExamination.js
+++ src/components/PageComponent/Medical_Examination/MedicalExamination.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainLayout from '../../Layout/MainLayout';
 
+import { FaMapMarkerAlt } from 'react-icons/fa';
+import { AiOutlineDown } from 'react-icons/ai';
+
 const MedicalExamination = () => {
+  const [ isShow, setIsShow ] = useState(false);
+
+  const SeletBox = () => {
+    if(isShow){
+      return(
+        <div className='absolute w-[27vw] h-[30vh] top-[6vh] z-10 rounded-md border-[1px] bg-[#FFFFFF]'>
+          
+        </div>
+      )
+    }
+  }
+
   return (
     <MainLayout>
-      MedicalExamination
+      <div className='absolute w-[71.5vw] h-[12vh] left-[1vw] top-[2vh] rounded-md bg-[#FFFFFF]'>
+        <div className='flex flex-row gap-[1vw] absolute h-[5vh] left-[1vw] top-[1vh]'>
+          <button
+            className='w-[27vw] h-[5vh] rounded-md border-[#707070] border-[0.5px]'
+            onClick={() => setIsShow(!isShow)}
+          > 
+            <div className='flex flex-row gap-[1vw]'>
+              <FaMapMarkerAlt className='text-[20px] text-[#465F85] ml-[6vw]' />
+              <p>시/도 &gt; 시/군/구 &gt; 읍/면/도</p>
+              <AiOutlineDown className='text-[18px] h-[3vh] ml-[4vw]' />
+            </div>
+          </button>
+          <SeletBox />
+          <input type='text' placeholder='검진기관명 입력' className='w-[27vw] h-[5vh] pl-[0.5vw] rounded-md border-[#707070] border-[0.5px]'/>
+        </div>
+        <div className='absolute w-[5vw] h-[5vh] left-[1vw] top-[6vh]'>
+          
+        </div>  
+      </div>
+      <div className='absolute w-[71.5vw] h-[51vh] left-[1vw] top-[15vh] rounded-md bg-[#FFFFFF]'>
+      </div>
     </MainLayout>
   )
 }
--- src/components/PageComponent/Medicine/Medicine.js
+++ src/components/PageComponent/Medicine/Medicine.js
@@ -1,10 +1,110 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainLayout from '../../Layout/MainLayout';
+import MedicineSearchBox from './MedicineSearchBox';
+
+import { BsFillCircleFill } from 'react-icons/bs';
+import { AiOutlineDown } from 'react-icons/ai';
 
 const Medicine = () => {
+  const DiseaseData = [
+    {
+      name : '당뇨',
+      color: "#465F85",
+      isShow: true
+    },
+    {
+      name : '고혈압',
+      color: "#844685",
+      isShow: false
+    },
+    {
+      name : '비염',
+      color: "#84A0CD",
+      isShow: false
+    },
+    {
+      name : '우울증',
+      color: "#68878F",
+      isShow: false
+    },
+  ]
+  
+  const MyMedicineInfo = ({ disease }) => {
+    const [ isShow, setIsShow ] = useState(false);
+
+    if(isShow){
+      return(
+        <div className='flex flex-wrap w-[100%] h-[30vh] bg-[#EEE6E2] rounded-md mb-1'>
+          <div className='flex flex-row w-[100%] h-[7vh] items-center gap-[1vw]'>
+            <BsFillCircleFill className='text-[4.5vh] h-[7vh] ml-[1vw]' style={{ color: disease.color }}/>
+            <div className='w-[62vw]'>
+              <p className='text-[14px]'>{ disease.name }</p>
+              <p className='text-[10px] text-[#708099]'>2시간 남았습니다</p>
+            </div>
+            <button onClick={() => setIsShow(!isShow)}>
+              <AiOutlineDown className='text-[16px]' />
+            </button>
+          </div>
+          <div className='w-[67.5vw] h-[22vh] ml-[1vw] mb-[1vh] pt-[1vh] pl-[1vw] rounded-sm bg-white overflow-scroll'>
+            <div className='flex flex-row items-center w-[65.5vw] h-[8vh] gap-[1vw] mb-[1vh] drop-shadow rounded-sm bg-[#FBF7F4]'>
+              <div className='w-[80px] h-[45px] ml-[1vw] text-center leading-[45px] bg-slate-400'>
+                <p className='text-[10px]'>약 이미지</p>
+              </div>
+              <div className=''>
+                <p className='font-semibold text-sm'>제품명</p>
+                <p className='text-[#8C8C8C] text-sm'>약의 성분과 함량에 대한 정보</p>
+              </div>
+            </div>
+            <div className='flex flex-row items-center w-[65.5vw] h-[8vh] gap-[1vw] mb-[1vh] drop-shadow rounded-sm bg-[#FBF7F4]'>
+              <div className='w-[80px] h-[45px] ml-[1vw] text-center leading-[45px] bg-slate-400'>
+                <p className='text-[10px]'>약 이미지</p>
+              </div>
+              <div className=''>
+                <p className='font-semibold text-sm'>제품명</p>
+                <p className='text-[#8C8C8C] text-sm'>약의 성분과 함량에 대한 정보</p>
+              </div>
+            </div>
+            <div className='flex flex-row items-center w-[65.5vw] h-[8vh] gap-[1vw] mb-[1vh] drop-shadow rounded-sm bg-[#FBF7F4]'>
+              <div className='w-[80px] h-[45px] ml-[1vw] text-center leading-[45px] bg-slate-400'>
+                <p className='text-[10px]'>약 이미지</p>
+              </div>
+              <div className=''>
+                <p className='font-semibold text-sm'>제품명</p>
+                <p className='text-[#8C8C8C] text-sm'>약의 성분과 함량에 대한 정보</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    }
+
+    return(
+      <div className='w-[100%] h-[7vh] bg-[#EEE6E2] rounded-md mb-1'>
+        <div className='flex flex-row items-center gap-[1vw] absolute left-[1vw]'>
+          <BsFillCircleFill className='text-[4.5vh] h-[7vh]' style={{ color: disease.color}} />
+          <div className='w-[62vw]'>
+            <p className='text-[14px]'>{ disease.name }</p>
+            <p className='text-[10px] text-[#708099]'>2시간 남았습니다</p>
+          </div>
+          <button onClick={() => setIsShow(!isShow)}>
+            <AiOutlineDown className='text-[16px]' />
+          </button>
+        </div>
+      </div>
+    )
+  }
   return (
     <MainLayout>
-      Medicine
+      <MedicineSearchBox />
+      <div className='absolute w-[71.5vw] h-[52vh] left-[1vw] top-[14vh] rounded-md bg-[#FFFFFF]'>
+        <div className='absolute h-[50vh] w-[69.5vw] top-[1vh] left-[1vw] overflow-scroll'>
+          { DiseaseData.map(( disease, idx ) => {
+            return(
+              <MyMedicineInfo key={ idx } disease={ disease } id={ idx }/>
+            )
+          })}
+        </div>
+      </div>
     </MainLayout>
   )
 }
