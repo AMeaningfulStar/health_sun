b1900e59d038f9d8e4d4b203dbd9b2dc7ce7ecba
A Meaningful star <ghrlfehd123@naver.com>
2023-04-27 15:58:07 +0900

    FEAT: 카카오맵 연동 및 페이지 퍼블리싱
    23.04.27
    
    - 카카오맵 연동을 위한 KakaoMap 컴포넌트 구현
    - Modal 컴포넌트로 만들어 각 페이지에 적용

--- src/components/PageComponent/Medicine/MedicineSearchBox.js
+++ src/components/PageComponent/Medicine/MedicineSearchBox.js
@@ -1,12 +1,7 @@
 import React, { useState } from 'react';
-import ReactModal from 'react-modal';
-import ColorData from './SearchData/ColorData';
-import ShapeData from './SearchData/ShapeData';
-import TypeData from './SearchData/TypeData';
-import LineData from './SearchData/LineData';
+import MedicineModal from './MedicineModal';
 
 import { BiSearch } from 'react-icons/bi';
-import { AiOutlineClose } from 'react-icons/ai';
 
 const MedicineSearchBox = () => {
   const [ isShow, setIsShow ] = useState(false);
@@ -25,111 +20,7 @@ const MedicineSearchBox = () => {
       </div>
       <div className='absolute w-[auto] top-[6.5vh] left-[1vw] '>
         <button className='text-[15px] text-[#969595] font-semibold hover:text-[#707070]' onClick={() => setIsShow(true)}>[모양으로 검색하기]</button>
-        <ReactModal
-          isOpen={isShow}
-          onRequestClose={() => setIsShow(false)}
-          className='absolute w-[35vw] h-[80vh] left-[34vw] top-[10vh] rounded-md bg-[#EEE6E2]'
-        > 
-          <div className='absolute left-[1.5vw] top-[2vh] flex flex-row gap-[40vh] items-center'>
-            <p className='text-[20px] text-[#707070] font-bold'>모양으로 검색하기</p>
-              <button>
-              <AiOutlineClose className='text-[23px] text-[#707070]' onClick={() => setIsShow(false)} />
-            </button>
-          </div>
-          <div className='absolute w-[33vw] h-[71vh] left-[1vw] top-[7vh] rounded-md bg-[#FFFFFF] flex flex-col'>
-            <div className='w-[100%] h-[15.5vh] mb-1'>
-              <p className='absolute left-[1vw] pt-[1vh] text-[13px] text-[#c9c7c7] font-semibold'>색상</p>
-              <div className='absolute w-[31vw] h-0 mt-[3.2vh] left-[1vw] border-[0.5px] border-solid border-[#D2D2D2]'/>
-              <div className='absolute w-[31vw] h-[11vh] mt-[4vh] left-[1vw] flex flex-row gap-x-[0.2vw]'>
-                <div className='w-[27vw] h-[11vh] flex flex-row flex-wrap gap-x-[0.4vw] gap-y-[0.4vw]'>
-                  { ColorData.map((color, idx) => {
-                    return (
-                      <button key={idx} className='w-[3vw] h-[5vh] rounded-sm text-center border-[0.5px] border-[#D2D2D2] hover:border-[#DB8888] hover:border-[1px]'>
-                        <div className='h-[2.5vh] rounded-sm border-b-[0.05px]' style={{ backgroundColor : `${ color.color }`}}/>
-                        <p className='text-[14px] text-[#707070]'>{ color.name }</p>
-                      </button>
-                    )
-                  })}
-                </div>
-                <button className='w-[4vw] rounded-sm text-center border-[0.5px] border-[#EEE6E2] hover:border-[#DB8888] hover:border-[1px]'>
-                  전체
-                </button>
-              </div>
-            </div>
-            <div className='w-[100%] h-[20vh] mb-1'>
-              <p className='absolute left-[1vw] pt-[1vh] text-[13px] text-[#c9c7c7] font-semibold'>모양</p>
-              <div className='absolute w-[31vw] h-0 mt-[3.2vh] left-[1vw] border-[0.5px] border-solid border-[#D2D2D2]'/>
-              <div className='absolute w-[31vw] h-[16vh] mt-[4vh] left-[1vw] flex flex-row flex-wrap gap-x-[0.2vw] gap-y-[0.2vh]'>
-                { ShapeData.map(( shape, idx) => {
-                  return(
-                    <button key={idx} className='w-[5vw] h-[8vh] rounded-sm text-center border-[0.5px] border-[#EEE6E2] hover:border-[#DB8888] hover:border-[1px]'>
-                      <div className='flex flex-wrap gap-[0.3vw] justify-center'>
-                        <img src={shape.img} alt='img' className='mt-[0.6vh]'/>
-                        <p className='text-[13px] text-[#707070]'>{ shape.name }</p>
-                      </div>
-                    </button>
-                  )
-                })}
-                <button className='w-[5vw] h-[8vh] rounded-sm text-center border-[0.5px] border-[#EEE6E2] hover:border-[#DB8888] hover:border-[1px]'>
-                  <p className='text-[13px] text-[#707070]'>기타</p>
-                </button>
-                <button className='w-[5vw] h-[8vh] rounded-sm text-center border-[0.5px] border-[#EEE6E2] hover:border-[#DB8888] hover:border-[1px]'>
-                  <p className='text-[13px] text-[#707070]'>전체</p>
-                </button>
-              </div>
-            </div>
-            <div className='w-[100%] h-[13.5vh] mb-1'>
-              <p className='absolute left-[1vw] pt-[1vh] text-[13px] text-[#c9c7c7] font-semibold'>제형</p>
-              <div className='absolute w-[31vw] h-0 mt-[3.2vh] left-[1vw] border-[0.5px] border-solid border-[#D2D2D2]'/>
-              <div className='absolute w-[31vw] h-[9vh] mt-[4vh] left-[1vw] flex flex-row gap-x-[0.3vw]'>
-                { TypeData.map(( type, idx ) => {
-                  return(
-                    <button key={idx} className='w-[6vw] h-[9vh] rounded-sm text-center border-[0.5px] border-[#EEE6E2] hover:border-[#DB8888] hover:border-[1px]'>
-                      <div className='flex flex-wrap gap-x-[1.3vw] gap-y-[0.3vw] justify-center'>
-                        <img src={type.img} alt='img' className='mt-[0.6vh]'/>
-                        <p className='text-[13px] text-[#707070]'>{ type.name }</p>
-                      </div>
-                    </button>
-                  )
-                })}
-                <button className='w-[6vw] h-[9vh] rounded-sm text-center border-[0.5px] border-[#EEE6E2] hover:border-[#DB8888] hover:border-[1px]'>
-                  <p className='text-[13px] text-[#707070]'>기타</p>
-                </button>
-                <button className='w-[6vw] h-[9vh] rounded-sm text-center border-[0.5px] border-[#EEE6E2] hover:border-[#DB8888] hover:border-[1px]'>
-                  <p className='text-[13px] text-[#707070]'>전체</p>
-                </button>
-              </div>
-            </div>
-            <div className='w-[100%] h-[13.5vh] mb-1'>
-              <p className='absolute left-[1vw] pt-[1vh] text-[13px] text-[#c9c7c7] font-semibold'>분할선</p>
-              <div className='absolute w-[31vw] h-0 mt-[3.2vh] left-[1vw] border-[0.5px] border-solid border-[#D2D2D2]'/>
-              <div className='absolute w-[31vw] h-[9vh] mt-[4vh] left-[1vw] flex flex-row gap-x-[0.3vw]'>
-                { LineData.map(( line, idx ) => {
-                  return(
-                    <button key={idx} className='w-[6vw] h-[9vh] rounded-sm text-center border-[0.5px] border-[#EEE6E2] hover:border-[#DB8888] hover:border-[1px]'>
-                      <div className='flex flex-wrap gap-x-[1.3vw] gap-y-[0.3vw] justify-center'>
-                        <img src={line.img} alt='img' className='mt-[0.6vh]'/>
-                        <p className='text-[13px] text-[#707070]'>{ line.name }</p>
-                      </div>
-                    </button>
-                  )
-                })}
-                <button className='w-[6vw] h-[9vh] rounded-sm text-center border-[0.5px] border-[#EEE6E2] hover:border-[#DB8888] hover:border-[1px]'>
-                  <p className='text-[13px] text-[#707070]'>기타</p>
-                </button>
-                <button className='w-[6vw] h-[9vh] rounded-sm text-center border-[0.5px] border-[#EEE6E2] hover:border-[#DB8888] hover:border-[1px]'>
-                  <p className='text-[13px] text-[#707070]'>전체</p>
-                </button>
-              </div>
-            </div>
-            <div className='w-[100%]'>
-              <button className='absolute w-[10vw] h-[5vh] left-[12vw] items-center bg-[#DB8888] rounded-md flex flex-row gap-[1vw]' onClick={() => setIsShow(false)}>
-                <BiSearch className='text-[25px] text-[#FFFFFF] ml-[2vw]'/>
-                <p className='text-[20px] text-[#FFFFFF] font-bold'>검색</p>
-              </button>
-            </div>  
-          </div>
-        </ReactModal>
+        <MedicineModal isShow={ isShow } setIsShow={ setIsShow } />
       </div>
     </div>
   )
--- src/components/PageComponent/Position/Position.js
+++ src/components/PageComponent/Position/Position.js
@@ -1,27 +1,26 @@
 import React, { useState } from 'react';
 import MainLayout from '../../Layout/MainLayout';
-
-// 임시
-import Map from '../../DesignFile/Map.png'
+import KakaoMap from '../../KakaoMap/KakaoMap';
 
 const Position = () => {
   const [ isShow, setIsShow ] = useState(false);
 
   const SearchBox = () => {
     if(isShow){
-      return <div className='absolute w-[30vw] h-[58vh] left-[1vw] top-[8vh] bg-white rounded-md drop-shadow-lg'></div>
+      return <div className='absolute w-[30vw] h-[58vh] left-[1vw] top-[8vh] bg-white rounded-md drop-shadow-lg z-10'></div>
     }
   }
 
   return (
     <MainLayout>
-      <img src={Map} alt='exemple' className='w-[73.5vw] h-[68vh] rounded-lg'/>
-      <div className='absolute flex flex-row gap-x-[1vw] left-[1vw] top-[1vh] w-[71.5vw]'>
-        <div className='w-[18vw] h-[5vh] bg-[#DCA3A3] rounded-md hover:bg-[#DB8888] leading-[5vh] text-center text-[#FFFFFF] drop-shadow-lg font-bold text-[0.9rem]' onClick={() => setIsShow(!isShow)}>건강 지도 보기</div>
-        <div className='w-[18vw] h-[5vh] bg-[#DCA3A3] rounded-md hover:bg-[#DB8888] leading-[5vh] text-center text-[#FFFFFF] drop-shadow-lg font-bold text-[0.9rem]' onClick={() => setIsShow(!isShow)}>병원 및 약국 종류별 찾기</div>
-        <div className='w-[18vw] h-[5vh] bg-[#DCA3A3] rounded-md hover:bg-[#DB8888] leading-[5vh] text-center text-[#FFFFFF] drop-shadow-lg font-bold text-[0.9rem]' onClick={() => setIsShow(!isShow)}>우리 동네 좋은 병원 찾기</div>
-        <div className='w-[18vw] h-[5vh] bg-[#DCA3A3] rounded-md hover:bg-[#DB8888] leading-[5vh] text-center text-[#FFFFFF] drop-shadow-lg font-bold text-[0.9rem]' onClick={() => setIsShow(!isShow)}>전문 병원 찾기</div>
-      </div>
+      <KakaoMap>
+        <div className='absolute flex flex-row gap-x-[1vw] left-[1vw] top-[1vh] w-[71.5vw] z-10'>
+          <div className='w-[18vw] h-[5vh] bg-[#DCA3A3] rounded-md hover:bg-[#DB8888] leading-[5vh] text-center text-[#FFFFFF] drop-shadow-lg font-bold text-[0.9rem]' onClick={() => setIsShow(!isShow)}>건강 지도 보기</div>
+          <div className='w-[18vw] h-[5vh] bg-[#DCA3A3] rounded-md hover:bg-[#DB8888] leading-[5vh] text-center text-[#FFFFFF] drop-shadow-lg font-bold text-[0.9rem]' onClick={() => setIsShow(!isShow)}>병원 및 약국 종류별 찾기</div>
+          <div className='w-[18vw] h-[5vh] bg-[#DCA3A3] rounded-md hover:bg-[#DB8888] leading-[5vh] text-center text-[#FFFFFF] drop-shadow-lg font-bold text-[0.9rem]' onClick={() => setIsShow(!isShow)}>우리 동네 좋은 병원 찾기</div>
+          <div className='w-[18vw] h-[5vh] bg-[#DCA3A3] rounded-md hover:bg-[#DB8888] leading-[5vh] text-center text-[#FFFFFF] drop-shadow-lg font-bold text-[0.9rem]' onClick={() => setIsShow(!isShow)}>전문 병원 찾기</div>
+        </div>
+      </KakaoMap>
       <SearchBox />
     </MainLayout>
   )
